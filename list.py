1a7b78bcdf5ef88f59a80bf7aa70f3e6059b4246
gali pomeranchik <g0583220911@gmail.com>
2025-03-09 20:20:48 +0200

    list code

---
+++ list.py
@@ -0,0 +1,196 @@
+#1
+age=[14,14,15,19,22]
+#2
+age.append(17)
+#3
+print(age[2])
+#4
+if 14 in age:
+    age.remove(14)
+#5
+age.remove(14)
+#6
+age2=[88,91,83,79]
+ages=age2+age
+#7
+agesNew=[]
+for num1 in range(len(ages)):
+        agesNew.append(num1);
+        if ages[num1] in ages[num1 + 1:ages(len)]:
+         ages.remove(num1);
+#8
+for num1 in range(len(ages)):
+ print[num1]
+#9
+print(ages(len))
+#10
+for num1 in range(len(ages)):
+ del ages[0]
+#11
+frut=['orange','aplle']
+#12
+frut[0]='orange'
+#13
+if 'banana' in frut:
+    frut.remove('banana')
+#14
+mat=[[1,2,3]
+     [3,4,5]
+     [6,7,8]]
+
+#16
+del mat[1]
+#17
+matg=mat
+#18
+sum=0
+endsum=0
+for num1 in range(len(mat)):
+    for num2 in range(len(mat[num1])):
+        sum+=mat[num1[num2]]
+        if(sum>endsum):
+            endsum=sum
+print(endsum)
+#19
+sum=0
+for num1 in range(len(mat)):
+    for num2 in range(len(mat[num1])):
+        sum+=mat[num1[num2]]
+print(sum)
+#20
+sort=[]
+for num1 in range(len(ages)):
+    for num2 in range(len(ages)):
+        if(ages[num2]>max):
+            max=ages[num2]
+            i=num2
+            sort.append(max)
+    ages[i]=-1
+    max=0
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
